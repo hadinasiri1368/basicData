6678d59f47698fe7c6fecaafb46619cfbf06b38c
h.nasiri <h.nasiri@rayanhamafza.com>
2023-12-02 16:37:01 +0330

    check token validation

--- src/main/java/org/basicData/api/FleetTypeAPI.java
+++ src/main/java/org/basicData/api/FleetTypeAPI.java
@@ -1,5 +1,6 @@
 package org.basicData.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.basicData.common.CommonUtils;
 import org.basicData.model.FleetType;
 import org.basicData.service.GenericService;
@@ -12,28 +13,30 @@ import java.util.List;
 public class FleetTypeAPI {
     @Autowired
     private GenericService<FleetType> service;
+
     @PostMapping(path = "/api/fleetType/add")
-    public Long addPerson(@RequestBody FleetType fleetType){
-        Long userId = CommonUtils.getUserId(null);
+    public Long addPerson(@RequestBody FleetType fleetType, HttpServletRequest request) {
+        Long userId = CommonUtils.getUserId(CommonUtils.getToken(request));
         service.insert(fleetType, userId);
         return fleetType.getId();
     }
+
     @PostMapping(path = "/api/fleetType/edit")
-    public Long editPerson(@RequestBody FleetType fleetType) {
-        Long userId = CommonUtils.getUserId(null);
+    public Long editPerson(@RequestBody FleetType fleetType, HttpServletRequest request) {
+        Long userId = CommonUtils.getUserId(CommonUtils.getToken(request));
         service.update(fleetType, userId);
         return fleetType.getId();
     }
 
     @PostMapping(path = "/api/fleetType/remove/{id}")
     public Long removePerson(@PathVariable Long id) {
-        service.delete(new FleetType(id,null));
+        service.delete(new FleetType(id, null));
         return id;
     }
 
     @GetMapping(path = "/api/fleetType/{id}")
     public FleetType getPerson(@PathVariable Long id) {
-        return service.findOne(FleetType.class,id);
+        return service.findOne(FleetType.class, id);
     }
 
     @GetMapping(path = "/api/fleetType")
