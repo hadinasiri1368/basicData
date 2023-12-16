2fbdbeca91945ce39d30041c948244b56897def0
pouriya <pourya.ahz@gmail.com>
2023-12-17 01:12:34 +0330

    edit PlaqueTagPersianPartAPI and PlaqueTagPersianPart

--- src/main/java/org/basicData/dto/CarGroupDto.java
+++ src/main/java/org/basicData/dto/CarGroupDto.java
@@ -1,10 +1,8 @@
 package org.basicData.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.basicData.model.CarCapacity;
 
 @AllArgsConstructor
 @Getter
